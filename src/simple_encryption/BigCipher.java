5adad5fc847643336ea7931d1fd817e9ed497fd3
jakef <jakefredjones@gmail.com>
2025-08-16 14:53:50 -0500

    Initial commit

---
+++ src/simple_encryption/BigCipher.java
@@ -0,0 +1,9 @@
+package simple_encryption;
+
+public class BigCipher {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/simple_encryption/Substitution_cipher.java
+++ src/simple_encryption/Substitution_cipher.java
@@ -1,5 +1,19 @@
 package simple_encryption;
 
 public class Substitution_cipher {
-
+	// takes a plaintext as input and outputs key and ciphertext
+	public String[] substitute(String plaintext) {
+		// initializes a string array with space for two strings
+		String[] cipherAndKey = new String[2];
+		
+		return cipherAndKey;
+	}
+	// returns a random substitution key with all ASCII characters
+	public String makeSubstitutionKey() {
+		// imports all ASCII characters as x
+		String x = Alphabet.CHARACTERS;
+		String key = "";
+		
+		return key;
+	}
 }
