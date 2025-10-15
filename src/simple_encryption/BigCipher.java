088aa103a2138b813b9d54e02c5f42e602548fb7
jakef <jakefredjones@gmail.com>
2025-10-14 21:35:33 -0500

    comments_fixmes

--- src/simple_encryption/BigCipher.java
+++ src/simple_encryption/BigCipher.java
@@ -3,9 +3,17 @@ package simple_encryption;
 public class BigCipher {
 
 	public static void main(String[] args) {
+		String message = "a";
+		
+		// creates new substitution cipher object called cipher
 		Substitution_cipher cipher = new Substitution_cipher();
+		// makes a substitution key 
 		String key = cipher.makeSubstitutionKey();
 		System.out.println(key);
+		
+		String encryptedMessage = cipher.encrypt(key, message);
+		
+		System.out.println(encryptedMessage);
 	}
 
 }
--- src/simple_encryption/Substitution_cipher.java
+++ src/simple_encryption/Substitution_cipher.java
@@ -10,16 +10,21 @@ public class Substitution_cipher {
 		
 		return cipherAndKey;
 	}
-	// returns a random substitution key with all ASCII characters
+	// returns key as a string with all of the ASCII characters in random order
 	public String makeSubstitutionKey() {
 		// imports all ASCII characters as letters
 		ArrayList<String> letters = new ArrayList<>(Alphabet.CHARACTER_LIST);
 		String key = "";
 		Random rand = new Random();
 		
+		// runs until the letters arraylist runs out
+		// makes the key
 		while (!letters.isEmpty()) {
+			// assigns a random number within scope of letters
 			int nextLetterNum = rand.nextInt(letters.size());
+			// gets next letter based on random int
 			String nextLetter = letters.get(nextLetterNum);
+			// adds letter to key and removes from letters
 			key += nextLetter;
 			letters.remove(nextLetterNum);
 		}
@@ -27,4 +32,24 @@ public class Substitution_cipher {
 		
 		return key;
 	}
+	
+	public String encrypt(String key, String message) {
+		String encryptedMessage = "";
+		
+		for (int i = 0; i < message.length(); i++) {
+			// getting character from message string
+			char toEncode = message.charAt(i);
+			
+			// converting character to numerical value, this will be the position of the key
+			// the first 31 ASCII characters are non-printable and therefore cannot show up in the message
+			int charNum = Character.getNumericValue(toEncode) - 31;
+			
+			// appending encrypted letter to message
+			encryptedMessage += Alphabet.CHARACTER_LIST.get(charNum);
+		}
+		
+		return encryptedMessage;
+	}
+	
+	// #FIXME: make a method to decrypt the message
 }
